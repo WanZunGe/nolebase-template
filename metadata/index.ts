978bbf98d71ada962257e5f34dc5d3ca0425fbdf
WanZunGe <100510668+WanZunGe@users.noreply.github.com>
2024-07-09 21:46:45 +0800

    Update index.ts
    改名

--- metadata/index.ts
+++ metadata/index.ts
@@ -1,6 +1,6 @@
 /** 文本 */
-export const siteName = 'Nólëbase'
-export const siteShortName = 'Nólëbase'
+export const siteName = '黄海波'
+export const siteShortName = '黄海波'
 export const siteDescription = '记录回忆，知识和畅想的地方'
 
 /** 文档所在目录 */
